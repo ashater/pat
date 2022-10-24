1dda0666008524490ca70ca0ae80221f48e80a41
Ari Shater <ashater@gmail.com>
2022-10-24 17:03:29 -0400

    pyinstaller, search data frame, win services and network

--- scan_system.py
+++ scan_system.py
@@ -10,6 +10,9 @@ from typing import List
 import pandas as pd
 import streamlit as st
 import psutil
+from socket import getservbyname, getservbyport
+
+st.set_page_config(layout="wide")
 
 @st.cache
 def list_files(file_loc: str) -> List:
@@ -30,32 +33,82 @@ file_series, extension_series = list_files(folder)
 
 def list_processes():
     res = []
-    for proc in psutil.process_iter(['pid', 'name', 'username']):
+    for proc in psutil.process_iter(['pid', 'name', 'username', 'exe']):
         res.append(proc.info)
 
-    return pd.DataFrame(res).set_index('pid')
+    return pd.DataFrame(res)
+
+def get_network():
+    network_df = pd.DataFrame.from_dict(list(map(lambda x: { 'pid': str(x.pid), 'laddr': str(x.laddr), 
+                            'raddr': str(x.raddr), 'status': str(x.status), 'type': str(x.type), 
+                            'rport_type': map_port(x.raddr),
+                            'lport_type': map_port(x.laddr)} ,  
+                            psutil.net_connections(kind='inet'))))
+
+    #network_df.set_index('pid', inplace=True)
+    return network_df                            
+
+def map_port(addr):
+
+    if addr:
+        try:
+            return getservbyport(addr.port)
+        except OSError:
+            return ""
+    else:
+        return ""
+
+def get_win_services():
+    
+    services = []
+    for ser in psutil.win_service_iter():
+        services.append(ser.as_dict())
+
+    ret = pd.DataFrame.from_dict(services)
+
+    return ret
+    
 
 
+def search_frame(df, srch_str):
 
-st.title("🐞 Malware report! 🐞 ")
+    base = pd.Series([False] *df.shape[0])
+    for col in df.columns:
+        base|= df[col].astype('str').str.contains(srch_str)
+    
+    return df[base]
+
+
+st.title("🐞 System report! 🐞 ")
 col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("### Suspicious files")
     result = st.multiselect("Files to find", options=extension_series.values, help="find these files", 
-                            default=['.xlsx'])                
+                            default=['.cpp'])                
 
     mask = file_series.str.contains('🐞', regex = False)  # all false
     for res in result:
         mask = mask | file_series.str.contains(res, regex=False)
         
-
     st.dataframe(file_series[mask])
+
+    st.markdown("### Windows Services")
+    srch_val_sv = st.text_input('Search services table')
+    srv_df = get_win_services()
+    st.dataframe(search_frame(srv_df, srch_val_sv))
 
 
 with col2:
     st.markdown("### Running processes")
-    srch_val = st.text_input('Search process name')
+    srch_val_ps = st.text_input('Search process table')
     ps = list_processes()
-    mask = ps.name.str.contains(srch_val)
-    st.dataframe(ps[mask])
+    
+    st.dataframe(search_frame(ps, srch_val_ps))
+
+    st.markdown("### Network connections")
+    srch_val_nf = st.text_input('Search network connections')
+    nf_df = get_network()
+    st.dataframe(search_frame(nf_df, srch_val_nf))
+
+
